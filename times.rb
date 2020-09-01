8ab1f5136064f1ee4a84efa199dd469ad8aa489c
pleasant-communicator-3558 <sujchand2@gmail.com>
2020-09-01 03:03:39 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,6 +1,20 @@
 def using_times
 	#your code here
+	7.times do
+	  puts "Wingardium Leviosa"
+	 end
 end
+
+
+to define the helper_method
+
+def 
+  
+  7.times do
+    puts "Hi"
+  end
+  
+end 
 
 
 
